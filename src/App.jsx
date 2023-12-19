a80835b40db5f7b632b06e87fcb40df883a1db64
vandilsonbrito <vandilson25@outlook.com>
2023-12-19 15:27:50 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import About from "./components/About/About"
+import Banner from "./components/Banner/Banner"
+import Contato from "./components/Contato/Contato"
+import Projects from "./components/Projects/Projects"
+
+function App() {
+
+  return (
+    <>
+        <Banner/>
+        <About/>
+        <Projects/>
+        <Contato/>
+    </>
+  )
+}
+
+export default App
