1a5ac17b07c9feb66c270c3d0820b835d65955e9
vandilsonbrito <vandilson25@outlook.com>
2024-01-10 12:14:47 -0300

    Update styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-scroll";
 export default function Header() {
   return (
     <header className="w-full h-20 justify-center hidden lg:flex">
-        <nav className="w-1/2 h-20 flex items-center justify-between  bg-transparent absolute top-0 z-30 text-white">
+        <nav className="w-1/2 h-20 flex items-center justify-between  bg-transparent absolute top-2 z-30 text-white">
             <Link to='/Sobre' smooth={true} duration={500} className="cursor-pointer">SOBRE</Link>
             <Link to='/Projetos' smooth={true} duration={500} className="cursor-pointer">PROJETOS</Link>
             <Link to='/Contatos' smooth={true} duration={500} className="cursor-pointer">CONTATOS</Link>
